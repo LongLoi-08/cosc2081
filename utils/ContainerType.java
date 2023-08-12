3de7fed3ee20b67ef44610cee14bd5a246ecbd64
LongLoi-08 <s3758273@rmit.edu.vn>
2023-08-12 21:20:44 +0700

    update ContainerType with fuel comsumption rate for ship and truck

--- utils/ContainerType.java
+++ utils/ContainerType.java
@@ -1,19 +1,32 @@
 package utils;
 
 public enum ContainerType {
-    DRY_STORAGE("DRY STORAGE"),
-    OPEN_TOP("OPEN TOP"),
-    OPEN_SIDE("OPEN_SIDE"),
-    REFRIGERATED("REFRIGERATED"),
-    LIQUID("LIQUID");
+    DRY_STORAGE("DRY STORAGE", 3.5, 4.6),
+    OPEN_TOP("OPEN TOP", 2.8, 3.2),
+    OPEN_SIDE("OPEN_SIDE", 2.7, 3.2),
+    REFRIGERATED("REFRIGERATED", 4.5, 5.4),
+    LIQUID("LIQUID", 4.8, 5.3);
 
     private final String type;
+    private final double fuelConsumptionShip;
+    private final double fuelConsumptionTruck;
 
-    private ContainerType(String type) {
+    private ContainerType(String type, double fuelConsumptionShip, double fuelConsumptionTruck) {
         this.type = type;
+        this.fuelConsumptionShip = fuelConsumptionShip;
+        this.fuelConsumptionTruck = fuelConsumptionTruck;
     }
 
-    public String getType() {
+    public double getFuelConsumptionShip() {
+        return fuelConsumptionShip;
+    }
+
+    public double getFuelConsumptionTruck() {
+        return fuelConsumptionTruck;
+    }
+
+    @Override
+    public String toString() {
         return type;
     }
 }
