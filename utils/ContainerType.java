8df66a520c7fb38b14e53815469ba2920806768e
LongLoi-08 <s3758273@rmit.edu.vn>
2023-08-12 21:23:12 +0700

    update ContainerType for readability

--- utils/ContainerType.java
+++ utils/ContainerType.java
@@ -1,11 +1,11 @@
 package utils;
 
 public enum ContainerType {
-    DRY_STORAGE("DRY STORAGE", 3.5, 4.6),
-    OPEN_TOP("OPEN TOP", 2.8, 3.2),
-    OPEN_SIDE("OPEN_SIDE", 2.7, 3.2),
-    REFRIGERATED("REFRIGERATED", 4.5, 5.4),
-    LIQUID("LIQUID", 4.8, 5.3);
+    DRY_STORAGE ("DRY STORAGE"  , 3.5, 4.6),
+    OPEN_TOP    ("OPEN TOP"     , 2.8, 3.2),
+    OPEN_SIDE   ("OPEN_SIDE"    , 2.7, 3.2),
+    REFRIGERATED("REFRIGERATED" , 4.5, 5.4),
+    LIQUID      ("LIQUID"       , 4.8, 5.3);
 
     private final String type;
     private final double fuelConsumptionShip;
