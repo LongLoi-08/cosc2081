f129d4c89dfc49f2832aa6541ee3dbe65bf0cc6d
LongLoi-08 <s3758273@rmit.edu.vn>
2023-08-31 00:35:41 +0700

    rework the id generation and add a static array to maintain record of created objects

--- models/Container.java
+++ models/Container.java
@@ -4,13 +4,16 @@ import utils.ContainerType;
 import utils.VehicleType;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Container implements interfaces.Container {
-    private static int idCounter = 0;
+    private static ArrayList<Boolean> idCache = new ArrayList<>(Collections.nCopies(1000, false));
+    private static ArrayList<Container> containers = new ArrayList<>();
 
     private String id;
     private ContainerType containerType;
     private double weight;
+
 
     public Container() {}
 
@@ -18,24 +21,34 @@ public class Container implements interfaces.Container {
         this.id = id;
         this.containerType = containerType;
         this.weight = weight;
+
         // check for id that are inuse when read from file
-        if (Integer.parseInt(id.substring(1)) >= idCounter) {
-            idCounter = Integer.parseInt(id.substring(1)) + 1;
-        }
+        int idValue = Integer.parseInt(id.substring(1));
+        idCache.set(idValue, true);
+
+        // add obj to a static list
+        containers.add(this);
     }
 
-    protected Container(ContainerType containerType, double weight) {
+    public Container(ContainerType containerType, double weight) {
         this.id = generateId();
         this.containerType = containerType;
         this.weight = weight;
+        containers.add(this);
     }
 
-    // custom id incrementer
+
+    // custom id generator
     private String generateId() {
         String idFormat = "c%04d";
-        String newId = String.format(idFormat, idCounter);
-        idCounter++;
+        int idIndexValue = idCache.indexOf(false);
+        String newId = String.format(idFormat, idIndexValue + 1);
+        idCache.set(idIndexValue, true);
         return newId;
+    }
+
+    public static ArrayList<Container> getAllContainer() {
+        return containers;
     }
 
     public ArrayList<VehicleType> getUsableVehicle() {
@@ -57,8 +70,13 @@ public class Container implements interfaces.Container {
         return weight;
     }
 
-    @Override
+    @Override   // Currently not working, will solve later in the future
     public double getFuelConsumption(VehicleType vehicleType) {
         return containerType.getFuelConsumptionShip() * vehicleType.compareTo(VehicleType.SHIP) + containerType.getFuelConsumptionTruck() * vehicleType.compareTo(VehicleType.TRUCK);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s,%s,%.2f", id, containerType.toString(), weight);
     }
 }
