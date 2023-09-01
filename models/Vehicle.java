4a0b114915ee646cd62370a65be0bf0601d4a574
LongLoi-08 <s3758273@rmit.edu.vn>
2023-09-01 22:13:53 +0700

    update vehicle model with new id generator and static list

--- models/Vehicle.java
+++ models/Vehicle.java
@@ -3,21 +3,47 @@ package models;
 import utils.VehicleType;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Vehicle implements interfaces.Vehicle {
-    private static int idCounterT = 0;
-    private static int idCounterS = 0;
+    private static ArrayList<Boolean> idCacheT = new ArrayList<>(Collections.nCopies(1000, false));
+    private static ArrayList<Boolean> idCacheS = new ArrayList<>(Collections.nCopies(1000, false));
+    private static ArrayList<Vehicle> vehicles = new ArrayList<>();
 
     private String id;
     private VehicleType vehicleType;
     private double maxFuel;
     private double currentFuel;
     private double maxCarryCapacity;
-    private double currentCarryCapacity;
+    private double currentCarryLoad;
     private ArrayList<Container> containers;
-    private Port currentPort;   ////////////////////////////////////////
+    private Port currentPort;
 
     public Vehicle() {}
+
+    /**
+     * For dev purpose only!
+     * Use when importing mock data
+     */
+    public Vehicle(String id, VehicleType vehicleType, double maxFuel, double currentFuel, double maxCarryCapacity, double currentCarryWeight, ArrayList<Container> containers, Port currentPort) {
+        this.id = id;
+        this.vehicleType = vehicleType;
+        this.maxFuel = maxFuel;
+        this.currentFuel = currentFuel;
+        this.maxCarryCapacity = maxCarryCapacity;
+        this.currentCarryLoad = currentCarryWeight;
+        this.containers = containers;
+        this.currentPort = currentPort;
+
+        int idValue = Integer.parseInt(id.substring(1));
+        if (vehicleType == VehicleType.SHIP) {
+            idCacheS.set(idValue, true);
+        } else {
+            idCacheT.set(idValue, true);
+        }
+
+        vehicles.add(this);
+    }
 
     public Vehicle(VehicleType vehicleType, double maxFuel, double maxCarryCapacity, Port port) {
         this.id = generateId(vehicleType);
@@ -25,44 +51,37 @@ public class Vehicle implements interfaces.Vehicle {
         this.maxFuel = maxFuel;
         this.currentFuel = maxFuel;
         this.maxCarryCapacity = maxCarryCapacity;
-        this.currentCarryCapacity = 0;
+        this.currentCarryLoad = 0;
         this.containers = new ArrayList<>();
         this.currentPort = port;
+
+        vehicles.add(this);
     }
 
-    /**
-     * For dev purpose only!
-     * Use when importing mock data
-     */
-    public Vehicle(String id, VehicleType vehicleType, double maxFuel, double currentFuel, double maxCarryCapacity, double currentCarryCapacity, ArrayList<Container> containers, Port currentPort) {
-        this.id = id;
-        this.vehicleType = vehicleType;
-        this.maxFuel = maxFuel;
-        this.currentFuel = currentFuel;
-        this.maxCarryCapacity = maxCarryCapacity;
-        this.currentCarryCapacity = currentCarryCapacity;
-        this.containers = containers;
-        this.currentPort = currentPort;
+    public static ArrayList<Vehicle> getAllVehicles() {
+        return vehicles;
     }
 
     private String generateId(VehicleType vehicleType) {
-        String idFormatT = "tr%04d";
-        String idFormatS = "sh%04d";
+        String idFormatT = "tr%04d", idFormatS = "sh%04d";
+        int idIndexValue;
         String newId;
+
         if (vehicleType.equals(VehicleType.SHIP)) {
-            newId = String.format(idFormatS, idCounterS);
-            idCounterS++;
-        }
-        else {
-            newId = String.format(idFormatT, idCounterT);
-            idCounterT++;
+            idIndexValue = idCacheS.indexOf(false);
+            newId = String.format(idFormatS, idIndexValue + 1);
+            idCacheS.set(idIndexValue, true);
+        } else {
+            idIndexValue = idCacheT.indexOf(false);
+            newId = String.format(idFormatT, idIndexValue + 1);
+            idCacheT.set(idIndexValue, true);
         }
 
         return newId;
     }
 
     private boolean isLoadable(Container container) {
-        return container.getWeight() <= (maxCarryCapacity - currentCarryCapacity);
+        return container.getWeight() <= (maxCarryCapacity - currentCarryLoad);
     }
 
     @Override
@@ -96,8 +115,8 @@ public class Vehicle implements interfaces.Vehicle {
     }
 
     @Override
-    public double getCurrentCarryingCapacity() {
-        return currentCarryCapacity;
+    public double getCurrentCarryingLoad() {
+        return currentCarryLoad;
     }
 
     @Override
@@ -115,5 +134,13 @@ public class Vehicle implements interfaces.Vehicle {
         }
         System.out.println("Over carrying capacity alert!");
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "%s|%s|%.2f|%.2f|%.2f|%.2f|%d|%s|%s",
+                id, vehicleType.toString(), maxFuel, currentFuel, maxCarryCapacity, currentCarryLoad, containers.size(), containers, currentPort.getPortId()
+                );
     }
 }
