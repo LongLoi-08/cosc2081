2a6f1bf684326a27a8f093d6e930fe5a4c09ac04
LongLoi-08 <s3758273@rmit.edu.vn>
2023-09-18 21:08:12 +0700

    rework User class

--- models/User.java
+++ models/User.java
@@ -1,59 +1,124 @@
 package models;
 
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class User {
+    private static ArrayList<User> users = new ArrayList<>();
+
+    private static ArrayList<String> adminMenus = new ArrayList<String>() {{
+        // index 0: start menu
+       add("Admin Menu:\n" +
+               "[1] - List all Ports\n" +
+               "[2] - List all Containers\n" +
+               "[3] - List all Vehicles\n" +
+               "[4] - List all Users\n" +
+               "[5] - View Statistics\n" +
+               "[0] - Exit/Logout");
+    }};
+
+    private static ArrayList<String> managerMenus = new ArrayList<String>() {{
+        // index 0: start menu
+        add("Manager Menu:\n" +
+                "[1] - List all Ports\n" +
+                "[2] - List all Containers\n" +
+                "[3] - List all Vehicles\n" +
+                "[4] - List all Users\n" +
+                "[5] - View Statistics\n" +
+                "[0] - Exit/Logout");
+    }};
+
     private String username;
     private String password;
+    private boolean isAdmin = false;
+    private Port managerPort = null;
 
-    public User(String username, String password){
-        this.username=username;
-        this.password=password;
+    public User() {}
+
+    public User(String username, String password, boolean isAdmin, Port managerPort) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.managerPort = managerPort;
+        users.add(this);
     }
 
-    public void setUsername(String username){
-        this.username=username;
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public void setPassword(String password){
-        this.password=password;
+    public User setAsManager(Port port) {
+        this.managerPort = port;
+        users.add(this);
+        return this;
     }
 
-    public boolean login(String enteredUsername, String enteredPassword){
-        return this.username.equals(enteredUsername) && this.password.equals(enteredPassword);
-    }
-}
-
-class Admin extends User {
-    public Admin(String username, String password) {
-        super("admin", "1");
-    }
-    public void showAdminMenu(){
-        System.out.println("Admin Menu:");
-        System.out.println("1. Manage Ports");
-        System.out.println("2. Manage Containers");
-        System.out.println("3. View Statistics");
-        System.out.println("4. Exit");
-    }
-}
-
-class PortManager extends User {
-    private Port assignedPort;
-
-    public PortManager(Port assignedPort) {
-        super("manager1", "1");
-        this.assignedPort = assignedPort;
+    public User setAsAdmin() {
+        this.isAdmin = true;
+        users.add(this);
+        return this;
     }
 
-    public void showPortManagerMenu() {
-        System.out.println("Port Manager Menu:");
-        System.out.println("1. Manage Containers");
-        System.out.println("2. View Port Information");
-        System.out.println("3. Exit");
+    public ArrayList<User> getAllUsers() {
+        return users;
     }
 
-    public void viewPortInformation() {
-        System.out.println("Port Name: " + assignedPort.getName());
-        System.out.println("Port ID: " + assignedPort.getId());
+    public String getUsername() {
+        return username;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public Port getManagerPort() {
+        return managerPort;
+    }
+
+    public User authenticate(/*String username, String password*/) {
+        for (User user : users) {
+            if (Objects.equals(user.getUsername(), this.username) && Objects.equals(user.getPassword(), this.password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+//    public String showAdminMenu() {
+//        Scanner inputScanner = new Scanner(System.in);
+//
+//        CustomUtils.breakLn(5);
+//        System.out.println(adminMenus.get(0));
+//
+//        return inputScanner.nextLine();
+//    }
+//
+//    public String showManagerMenu() {
+//        Scanner inputScanner = new Scanner(System.in);
+//
+//        CustomUtils.breakLn(5);
+//        System.out.println("""
+//                Admin Menu:
+//                [1] - View Port information
+//                [2] - List all Port's Containers
+//                [3] - List all Port's Vehicles
+//                [4] - View Statistics
+//                """);
+//        System.out.println("[0] - Exit/Logout");
+//
+//
+//        return inputScanner.nextLine();
+//    }
+//
+//    public String showMenu() {
+//        if (this.isAdmin) return showAdminMenu();
+//        return showManagerMenu();
+//    }
 }
 
