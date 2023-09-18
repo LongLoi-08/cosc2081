f547dc62daa45cff8a72aaedd71d26c39ed50e57
LongLoi-08 <s3758273@rmit.edu.vn>
2023-09-18 21:09:05 +0700

    rework class and add new methods

--- models/PortManagementSystem.java
+++ models/PortManagementSystem.java
@@ -1,10 +1,119 @@
 package models;
+import utils.ContainerType;
+import utils.CustomUtils;
+import utils.VehicleType;
+
 import java.util.Scanner;
 
 public class PortManagementSystem {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static User login() {
+        CustomUtils.breakLn(5);
+
+        System.out.print("Enter your username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        return new User(username, password).authenticate();
+    }
+
+    private static boolean logout(User user, String input_) {
+        if (input_.equals("0")) {
+            user = null;
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void displayPort(User user) {
+        if (user.isAdmin()) {
+            for (Port port : new Port().getAllPorts()) {
+                System.out.println(port.toStringSaveFileFormat());
+            }
+            return;
+        }
+
+        System.out.println(user.getManagerPort().toStringSaveFileFormat());
+    }
+
+    private static void displayContainer(User user) {
+        if (user.isAdmin()) {
+            for (Container container : new Container().getAllContainer()) {
+                System.out.println(container.toStringSaveFileFormat());
+            }
+            return;
+        }
+
+        for (Container container : user.getManagerPort().getContainers()) {
+            System.out.println(container.toStringSaveFileFormat());
+        }
+    }
+
+    private static void displayVehicle(User user) {
+        if (user.isAdmin()) {
+            for (Vehicle vehicle : new Vehicle().getAllVehicles()) {
+                System.out.println(vehicle.toStringSaveFileFormat());
+            }
+            return;
+        }
+
+        for (Vehicle vehicle : user.getManagerPort().getVehicles()) {
+            System.out.println(vehicle.toStringSaveFileFormat());
+        }
+    }
+
+    private static void displayUser(User user) {
+        if (user.isAdmin()) {
+            for (User user_ : new User().getAllUsers()) {
+                if (!user_.isAdmin()) System.out.println(user_);
+            }
+            return;
+        }
+
+        System.out.println(user);
+    }
+
+    private static void displayMenuLayer0(User user) {
+        CustomUtils.breakLn(5);
+
+        if (user.isAdmin()) {
+            System.out.println("""
+                Admin Menu:
+                [1] - List all Ports
+                [2] - List all Containers
+                [3] - List all Vehicles
+                [4] - List all Users
+                [5] - View Statistics
+                [0] - Exit/Logout""");
+            return;
+        }
+
+        System.out.println("""
+                Manager Menu:
+                [1] - View Port information
+                [2] - List all Port's Containers
+                [3] - List all Port's Vehicles
+                [4] - View current User information
+                [4] - View Statistics
+                [0] - Exit/Logout""");
+    }
+
+    private static void displayMenuLayer1(User user, String string) {
+        CustomUtils.breakLn(5);
+        switch (string) {
+            case "1" -> displayPort(user);
+            case "2" -> displayContainer(user);
+            case "3" -> displayVehicle(user);
+            case "4" -> displayUser(user);
+            default -> System.out.println("Error! Undefined option.");
+        }
+    }
+
     public static void demo() {
-        Scanner scanner = new Scanner(System.in);
-//        Port assignedPort = new Port("P001", "Sample Port");
         Port assignedPort = new Port(
                 "Sample Port",
                 0,
@@ -12,81 +121,43 @@ public class PortManagementSystem {
                 100,
                 true
         );
-        PortManager portManager = new PortManager(assignedPort);
 
-        while (true) {
-            System.out.println("1. Admin Login");
-            System.out.println("2. Port Manager Login");
-            System.out.println("3. Exit");
-            System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
+        Vehicle vehicle = new Vehicle(VehicleType.TRUCK, 100.0, 200.0, assignedPort);
 
-            if (choice == 1) {
-                scanner.nextLine();
-                System.out.print("Enter admin username: ");
-                String adminUsername = scanner.nextLine();
-                System.out.print("Enter admin password: ");
-                String adminPassword = scanner.nextLine();
+        Container container1 = new Container(ContainerType.DRY_STORAGE, 10);
+        Container container2 = new Container(ContainerType.OPEN_TOP, 50);
 
-                Admin admin = new Admin(adminUsername,adminPassword);
-                if (admin.login(adminUsername, adminPassword)) {
-                    while (true) {
-                        admin.showAdminMenu();
-                        System.out.print("Enter your choice: ");
-                        int adminChoice = scanner.nextInt();
+        vehicle.loadContainer(container2);
 
+        assignedPort.getContainers().add(container1);
+        assignedPort.getVehicles().add(vehicle);
 
-                        if (adminChoice == 1) {
-                            System.out.println("1");
-                        } else if (adminChoice == 2) {
-                            System.out.println("2");
-                        } else if (adminChoice == 3) {
-                            System.out.println("3");
-                        } else if (adminChoice == 4) {
-                            break;
-                        } else {
-                            System.out.println("Invalid choice.");
-                        }
-                    }
+        User portManager_ = new User("manager", "pass").setAsManager(assignedPort);
+        User admin_ = new User("admin", "pass").setAsAdmin();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        do {
+            User user = login();
+            while (true) {
+                if (user == null) {
+                    System.out.println("Error! Incorrect username or password!\nPlease try again.");
+                    break;
                 } else {
-                    System.out.println("Invalid admin credentials.");
-                }
-            } else if (choice == 2) {
-                scanner.nextLine(); // Consume the newline character
-                System.out.print("Enter port manager username: ");
-                String managerUsername = scanner.nextLine();
-                System.out.print("Enter port manager password: ");
-                String managerPassword = scanner.nextLine();
+                    displayMenuLayer0(user);
 
-                if (portManager.login(managerUsername, managerPassword)) {
-                    while (true) {
-                        portManager.showPortManagerMenu();
-                        System.out.print("Enter your choice: ");
-                        int managerChoice = scanner.nextInt();
+                    System.out.println("Enter option: ");
+                    String inputString = scanner.nextLine();
 
-
-                        if (managerChoice == 1) {
-                            System.out.println("1");
-                        } else if (managerChoice == 2) {
-                            portManager.viewPortInformation();
-                        } else if (managerChoice == 3) {
-                            break;
-                        } else {
-                            System.out.println("Invalid choice.");
-                        }
+                    if (logout(user, inputString)) {
+                        System.out.println("GoodBye!");
+                        CustomUtils.pressEnterToContinue();
+                        break;
                     }
-                } else {
-                    System.out.println("Invalid port manager credentials.");
+
+                    displayMenuLayer1(user, inputString);
+                    CustomUtils.pressEnterToContinue();
                 }
-            } else if (choice == 3) {
-                System.out.println("Exiting the simulation.");
-                break;
-            } else {
-                System.out.println("Invalid choice.");
             }
-        }
-
-        scanner.close();
+        } while (true);
     }
 }
 
